ea3150b6be859917bcf8b2a2c56df3ee7493d74b
sya-dn <afizahdina585@gmail.com>
2024-11-20 00:34:50 -1200

    proyek umkm

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mr. Yess Cafe</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="logo-container">
+            <img src="img/logoo.png" alt="Logo Mr. Yess" class="logo">
+            <h1>Welcome to MR YESS COFFE CAFE</h1>
+        </div>
+        <div class="sub-header">
+            <h6>Buka setiap hari (08.00-14.00) (14.00-23.00)</h6>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+
+    <main>
+        <!-- Konten utama di sini -->
+        <section class="home-content">
+            <img src="img/tempat.jpeg" alt="Cafe Mr. Yess" class="cafe-image">
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Mr. Yess Coffe cafe</p>
+    </footer>
+</body>
+
+</html>
